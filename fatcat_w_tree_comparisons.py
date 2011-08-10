0afed48bba3feafd9f95118da99cf919fdaba0f7
Ajith Warrier <a.varier@gmail.com>
2011-08-10 12:02:30 -0700

    Modified the fatcat_w_tree_comparisons file to add the percent identities.

--- fatcat_w_tree_comparisons.py
+++ fatcat_w_tree_comparisons.py
@@ -2,13 +2,14 @@ import os
 import glob
 import subprocess
 from Bio import SeqIO
-from validation_scripts import compare_two_trees
+from validation_scripts import compare_two_trees, PID
 
 #Global variables for original destinations
 ORIGINAL_DATASET_FOLDER = os.path.join('FAT-CAT-1_dataset_validation', 'original_dataset')
 NEWICK_FILES_DIR = os.path.join(ORIGINAL_DATASET_FOLDER, 'gpcr_trees')
 SEQUENCE_FILES_DIR = os.path.join(ORIGINAL_DATASET_FOLDER, 'insert_seqs')
 ALN_FILES_DIR = os.path.join(ORIGINAL_DATASET_FOLDER, 'gpcr_mafft_alignments')
+ORIGINAL_MSA = os.path.join(ORIGINAL_DATASET_FOLDER, 'gpcr_nr95gc50_mafft.fasta')
 
 FINAL_DESTINATION = 'FAT-CAT_Output'
 
@@ -45,7 +46,8 @@ def run_comparisons(orig_tree, final_tree, inserted_leaf_name):
 
 def main():
     final_string = []
-    final_string.append('Deleted Sequence\tRF metric\tRF distance\tSOPD\tRF metric\tRF distance\tSOPD\tRF metric\tRF distance\tSOPD\tRF metric\tRF distance\tSOPD')
+    hpid = PID.Highest_PID_Match(ORIGINAL_MSA)
+    final_string.append('Deleted Sequence\tRF metric\tRF distance\tSOPD\tRF metric\tRF distance\tSOPD\tRF metric\tRF distance\tSOPD\tRF metric\tRF distance\tSOPD\tClosest Sequence (pwid)\tPWID score')
     for newickFile in glob.glob(NEWICK_FILES_DIR+ '/*'):
         file_name = os.path.basename(newickFile)
         file_name, ext = os.path.splitext(file_name)
@@ -64,13 +66,16 @@ def main():
         tree_n =  '%s_fat_cat/tree/tree_n.newick' % file_name
         tree_s ='%s_fat_cat/tree/tree_s.newick' % file_name
         inserted_leaf_name = get_leaf_name_from_seq_file(seq_file)
+        closest_seq_details = hpid.get_hpid_by_id(inserted_leaf_name)
         this_file_string = file_name;
         for tree in [final_tree,tree_plus,tree_n,tree_s]:
             rf_metric, rf_distance, sopd_distance = run_comparisons(orig_tree, tree, inserted_leaf_name)
             for var in [rf_metric, rf_distance, sopd_distance]:
                 this_file_string = this_file_string+'\t'+str(var)
+        this_file_string += ("\t%s\t%s" %
+                             (closest_seq_details['Highest scored id'],
+                              closest_seq_details['Highest score']))
         final_string.append(this_file_string)
-        
         print 'done'
     final_string = '\n'.join(final_string)
     f = open("FATCAT_STATS", 'w'); f.write(final_string); f.close()
